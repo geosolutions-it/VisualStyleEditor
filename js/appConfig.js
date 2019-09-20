295517cf7abc739c24abc0a514409bd5911dd1fe
allyoucanmap <bovio.stefano@gmail.com>
2019-09-20 12:28:39 +0200

    add not supported browser screen for ie

--- js/appConfig.js
+++ js/appConfig.js
@@ -5,6 +5,10 @@
  * This source code is licensed under the BSD-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { compose } from 'recompose';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import get from 'lodash/get';
 
 import HomePage from './pages/Home';
 import StylesAPIPage from './pages/StylesAPI';
@@ -12,27 +16,44 @@ import VisualStyleEditorPage from './pages/VisualStyleEditor';
 import TilesAPIPage from './pages/TilesAPI';
 import pluginsDef from './plugins';
 
+import emptyState from '@mapstore/components/misc/enhancers/emptyState';
+const selector = createSelector(
+    [
+        state => get(state, 'browser')
+    ], ({ ie }) => ({
+        notSupported: ie
+    })
+);
+
+const notSupportedScreen = compose(
+    connect(selector),
+    emptyState(({ notSupported }) => notSupported, {
+        title: 'Browser not supported',
+        glyph: 'exclamation-mark'
+    })
+);
+
 export default {
     pages: [
         {
             name: 'home',
             path: '/',
-            component: HomePage
+            component: notSupportedScreen(HomePage)
         },
         {
             name: 'styles-api',
             path: '/styles-api',
-            component: StylesAPIPage
+            component: notSupportedScreen(StylesAPIPage)
         },
         {
             name: 'visual-style-editor',
             path: '/visual-style-editor',
-            component: VisualStyleEditorPage
+            component: notSupportedScreen(VisualStyleEditorPage)
         },
         {
             name: 'tiles-api',
             path: '/tiles-api',
-            component: TilesAPIPage
+            component: notSupportedScreen(TilesAPIPage)
         }
     ],
     pluginsDef,
